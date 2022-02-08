dae1c79a14c212a9e0e9b929d6042f032d3f2367
rushi mungse <mungse.rushi@gmail.com>
2022-02-08 13:48:01 +0530

    datatype

---
+++ datatypes.js
@@ -0,0 +1,37 @@
+const colors = require("colors");
+console.log("Data types".bgMagenta);
+
+// 1.Primitive data types
+// 1.1Numbers
+
+/*
+let numbers = 12;
+let floatNum = 12.434;
+numbers = "Rushikesh"; // lossly typping
+console.log(numbers);
+*/
+
+//1.2.string
+let name = "Rushikesh Mungse";
+console.log(name);
+
+//1.3.Boolean
+let isLoggedIn = true;
+console.log(isLoggedIn);
+
+//1.4.undefined
+let age;
+console.log(age);
+
+//1.5.BigInt
+//1.6.symbols
+
+//2.Structural datatype
+//2.1 Object
+//   1.finctions
+//2.arrays
+//3.Map
+//4.Date
+
+//root-structure datatypes
+// null
